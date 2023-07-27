875e49fa584b87dd92a2d280105e631c8a7a1706
bschloss <ben.j.schloss@gmail.com>
2023-07-27 09:58:56 -0400

    Add langchain column mapping logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,21 @@
+import json
 import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 from utils import load_csv
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field, validator
+from typing import Dict, List
 
 # App title
 st.set_page_config(page_title="🤗💬 HugChat")
@@ -68,6 +82,55 @@ if csv_template is not None:
                 st.write(response)
         message = {"role": "assistant", "content": response}
         st.session_state.messages.append(message)
+
+
+if csv_target is not None:
+    template_columns = list(csv_template.columns)
+    target_columns = list(csv_target.columns)
+
+    # Set up Chat Model and store in session
+    OPEN_AI_KEY = json.load(open('/Users/benjaminschloss/llm-table-formatter/openai_key.json', 'r'))
+    TEMPERATURE = 0
+    MODEL = "gpt-3.5-turbo-0613"
+    chat_model = ChatOpenAI(
+        openai_api_key=OPEN_AI_KEY['OPEN_AI_KEY'],
+        temperature=TEMPERATURE,
+        model=MODEL
+    )
+    st.session_state['chat_model'] = chat_model
+
+    # Set up parser and store in session
+    class SuggestedMapping(BaseModel):
+        map: Dict[str, List[str]]
+    parser = PydanticOutputParser(pydantic_object=SuggestedMapping)
+    st.session_state['parser'] = parser
+
+    # Set up memory and store in session
+    st.session_state['memory'] = ConversationBufferMemory(memory_key="history", return_messages=True)
+
+    tmp = "You are a helpful assistant. Your job is to map columns in the template table to one or more "
+    tmp += " columns in the target table. The template table columns are: {template_columns}."
+    tmp += "\n{format_instructions}\n"
+    tmp += "Here are the target table columns: {target_columns}."
+    prompt = PromptTemplate.from_template(tmp)
+    input = prompt.format_prompt(
+        template_columns=', '.join(template_columns),
+        format_instructions=parser.get_format_instructions(),
+        target_columns=', '.join(target_columns)
+    )
+    qa = ConversationChain(llm=st.session_state['chat_model'], memory=st.session_state['memory'])
+    response = qa.run(input=input.to_string())
+    suggested_mapping = parser.parse(response)
+    with st.chat_message("assistant"):
+        temp_cols = ', '.join(template_columns)
+        targ_cols = ', '.join(target_columns)
+        mapping_text = json.dumps(suggested_mapping, indent=4)
+        response = f'I found the following columns in the template table:\n{temp_cols}\nAnd I found these columns'
+        response += f' in the other table you uploaded:\n{targ_cols}.\n'
+        response += f'Based on this information, I would suggest the following mapping:\n{mapping_text}'
+        st.write(response)
+
+
 
 
 # Function for generating LLM response

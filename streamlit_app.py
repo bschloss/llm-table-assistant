e7b95971b054590a4b1a3c4be81ae287da2f4dbe
bschloss <ben.j.schloss@gmail.com>
2023-07-27 15:47:49 -0400

    Logic Fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,11 +124,11 @@ if (
         st.session_state.template_df is not None
         and st.session_state.target_df is not None
 ):
-
-if not st.session_state.suggested_mapping:
     col1.dataframe(st.session_state.template_df)
     col2.dataframe(st.session_state.target_df)
 
+
+if not st.session_state.suggested_mapping:
     with sidebar.chat_message("assistant"):
         with st.spinner("Thank you. Please wait while I process your tables..."):
 
